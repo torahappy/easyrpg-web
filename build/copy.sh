72d643dfa86f90f72c6a7c48215d75db5fbf99ff
aidatorajiro <kawarusosu@zoho.com>
2024-08-18 14:15:34 +0900

    fix build

--- build/copy.sh
+++ build/copy.sh
@@ -1,2 +1,2 @@
 cp ./Player/output/bin/easyrpg-player.js ../www/index.js
-cp ./Player/output/bin/easyrpg-player.wasm ../www/index.wasm
+cp ./Player/output/bin/easyrpg-player.wasm ../www/easyrpg-player.wasm
