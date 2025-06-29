274f8ed44d7720b320f98432515dc2879bec8e0a
Tora Aida <kawarusosu@zoho.com>
2025-06-30 03:03:22 +0900

    api: add venv setup

--- api/start.bash
+++ api/start.bash
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-./venv/bin/python -m fastapi dev --port=9000 --host=0.0.0.0 server.py
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd "$SCRIPT_DIR"
+
+if [ ! -d ./venv ]; then
+  python3 -m venv venv
+  venv/bin/pip3 install -r ./requirements.txt
+fi
+
+venv/bin/python -m fastapi dev --port=9000 --host=0.0.0.0 server.py
