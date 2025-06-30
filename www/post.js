63c9dd1960949097a1a5cb02a55951fd9bb8354e
tora <kawarusosu@zoho.com>
2025-06-30 19:08:36 +0900

    web: prettify js

--- www/post.js
+++ www/post.js
@@ -4,155 +4,166 @@ let dbPrefix = urlParams.has("game") ? urlParams.get("game") + "/" : "";
 
 let debugstat = false;
 
-function getSaveFile (filename) {
+function getSaveFile(filename) {
   return new Promise((res, rej) => {
-    easyrpgPlayer.saveFs.loadLocalEntry('/easyrpg/' + dbPrefix + 'Save/' + filename, (fs_err, fs_res) => {
-      if (fs_err !== null) {
-          rej(fs_err)
-      } else {
-          res(fs_res)
-      }
-    })
-  })
+    easyrpgPlayer.saveFs.loadLocalEntry(
+      "/easyrpg/" + dbPrefix + "Save/" + filename,
+      (fs_err, fs_res) => {
+        if (fs_err !== null) {
+          rej(fs_err);
+        } else {
+          res(fs_res);
+        }
+      },
+    );
+  });
 }
 
-document.getElementById('debugimportone').addEventListener('click', function () {
-  const num = document.getElementById("debugnumber_importone").value;
-  if (!num.match(/^\d{1,2}$/)) {
-    alert("invalid save slot id")
-    return
-  }
-  easyrpgPlayer.api.uploadSavegame(num)
-})
+document
+  .getElementById("debugimportone")
+  .addEventListener("click", function () {
+    const num = document.getElementById("debugnumber_importone").value;
+    if (!num.match(/^\d{1,2}$/)) {
+      alert("invalid save slot id");
+      return;
+    }
+    easyrpgPlayer.api.uploadSavegame(num);
+  });
 
-document.getElementById('debugimport').addEventListener('click', function () {
-      let file = document.createElement('input');
-      file.type = 'file';
-      file.style.display = 'none';
-      file.addEventListener('change', async function (evt) {
-        const selected_file = evt.target.files[0];
-          let my_zip = JSZip();
-          let data = await my_zip.loadAsync(await selected_file.arrayBuffer());
-          for (let save of data.file(/^.+\.lsd$/)) {
-            let contents = await save.async("uint8array")
-            // let timestamp = new Date(save.date);
-            // let obj = {mode: 33206, contents, timestamp}
-            let slot = parseInt(save.name.match(/\d+/)[0])
+document.getElementById("debugimport").addEventListener("click", function () {
+  let file = document.createElement("input");
+  file.type = "file";
+  file.style.display = "none";
+  file.addEventListener("change", async function (evt) {
+    const selected_file = evt.target.files[0];
+    let my_zip = JSZip();
+    let data = await my_zip.loadAsync(await selected_file.arrayBuffer());
+    for (let save of data.file(/^.+\.lsd$/)) {
+      let contents = await save.async("uint8array");
+      // let timestamp = new Date(save.date);
+      // let obj = {mode: 33206, contents, timestamp}
+      let slot = parseInt(save.name.match(/\d+/)[0]);
       let buf = easyrpgPlayer._malloc(contents.length);
       easyrpgPlayer.HEAPU8.set(contents, buf);
       easyrpgPlayer.api_private.uploadSavegameStep2(slot, buf, contents.length);
       easyrpgPlayer._free(buf);
       easyrpgPlayer.api.refreshScene();
-          }
-      });
-    file.click();
+    }
+  });
+  file.click();
 });
 
-document.getElementById('debugexportone').addEventListener('click', function () {
-  const num = document.getElementById("debugnumber_exportone").value;
-  if (!num.match(/^\d{1,2}$/)) {
-    alert("invalid save slot id")
-    return
-  }
-  easyrpgPlayer.api.downloadSavegame(num)
-})
+document
+  .getElementById("debugexportone")
+  .addEventListener("click", function () {
+    const num = document.getElementById("debugnumber_exportone").value;
+    if (!num.match(/^\d{1,2}$/)) {
+      alert("invalid save slot id");
+      return;
+    }
+    easyrpgPlayer.api.downloadSavegame(num);
+  });
 
-document.getElementById('debugexec').addEventListener("click", () => {
-  let command = document.getElementById('debugta').value;
-  server_put_log(command)
+document.getElementById("debugexec").addEventListener("click", () => {
+  let command = document.getElementById("debugta").value;
+  server_put_log(command);
   try {
-    let r = JSON.stringify(eval(command))
-    alert(r)
-    server_put_log(r)
+    let r = JSON.stringify(eval(command));
+    alert(r);
+    server_put_log(r);
   } catch (e) {
-    alert(e)
-    server_put_log(e)
+    alert(e);
+    server_put_log(e);
   }
-})
+});
 
-document.getElementById('debugexport').addEventListener("click", async () => {
+document.getElementById("debugexport").addEventListener("click", async () => {
   let zip = new JSZip();
-  let rootDir = "backup-" + gameName + "-" + Date.now(); 
+  let rootDir = "backup-" + gameName + "-" + Date.now();
   let bak = zip.folder(rootDir);
   for (let i = 0; i < 100; i++) {
     let filename;
     if (i < 10) {
-      filename = "Save0" + i + ".lsd"
+      filename = "Save0" + i + ".lsd";
     } else {
-      filename = "Save" + i + ".lsd"
+      filename = "Save" + i + ".lsd";
     }
 
     try {
-      let sav = await getSaveFile(filename)
-      bak.file(filename, sav.contents, {date: sav.timestamp})
-    } catch (e) {
-    }
+      let sav = await getSaveFile(filename);
+      bak.file(filename, sav.contents, { date: sav.timestamp });
+    } catch (e) {}
   }
-  let content = await bak.generateAsync({type:"blob"})
-  
+  let content = await bak.generateAsync({ type: "blob" });
+
   saveAs(content, rootDir + ".zip");
-})
+});
 
-document.getElementById('reloadaudio').addEventListener('click', () => {
-  easyrpgPlayer.SDL2.audioContext.resume()
-})
+document.getElementById("reloadaudio").addEventListener("click", () => {
+  easyrpgPlayer.SDL2.audioContext.resume();
+});
 
-document.getElementById('debugbtninner').addEventListener("click", () =>{
+document.getElementById("debugbtninner").addEventListener("click", () => {
   if (debugstat) {
-    document.getElementById('debugbox-outer').style.display = "none"
-    debugbox.style.display = "none"
+    document.getElementById("debugbox-outer").style.display = "none";
+    debugbox.style.display = "none";
     canvas.focus();
   } else {
-    document.getElementById('debugbox-outer').style.display = "block"
-    debugbox.style.display = "block"
+    document.getElementById("debugbox-outer").style.display = "block";
+    debugbox.style.display = "block";
   }
 
-  debugstat = !debugstat
-})
+  debugstat = !debugstat;
+});
 
 async function syncSoundfontList() {
-  let result = await (await fetch('/api/list_soundfont')).json()
-  let el = document.getElementById('soundfontselection')
-  el.innerHTML = ''
-  let def = document.createElement('option')
-  def.innerText = 'default'
-  el.appendChild(def)
+  let result = await (await fetch("/api/list_soundfont")).json();
+  let el = document.getElementById("soundfontselection");
+  el.innerHTML = "";
+  let def = document.createElement("option");
+  def.innerText = "default";
+  el.appendChild(def);
   for (let i = 0; i < result.result.length; i++) {
-    let sfel = document.createElement('option')
-    sfel.innerText = result.result[i]
-    el.appendChild(sfel)
+    let sfel = document.createElement("option");
+    sfel.innerText = result.result[i];
+    el.appendChild(sfel);
   }
   if (result.current === null) {
-    el.selectedIndex = 0
+    el.selectedIndex = 0;
   } else {
-    el.selectedIndex = result.result.indexOf(result.current) + 1
+    el.selectedIndex = result.result.indexOf(result.current) + 1;
   }
 }
 
-syncSoundfontList()
+syncSoundfontList();
 
-document.getElementById('uploadsoundfont').addEventListener("click", (ev) =>{
-  easyrpgPlayer.api_private.createInputElement_js(undefined, async (file, name) => {
-    let formdata = new FormData()
-    formdata.append('file', new Blob([file.target.result]), name)
-    await fetch('/api/put_soundfont', { method: 'POST', body: formdata })
-    syncSoundfontList()
-  })
-})
-
-document.getElementById('soundfontselection').addEventListener("change", (ev) =>{
-  let el = document.getElementById('soundfontselection')
-  let filename
-  if (el.value === 'default') {
-    filename = null
-  } else {
-    filename = el.value
-  }
-  fetch('/api/set_soundfont', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
+document.getElementById("uploadsoundfont").addEventListener("click", (ev) => {
+  easyrpgPlayer.api_private.createInputElement_js(
+    undefined,
+    async (file, name) => {
+      let formdata = new FormData();
+      formdata.append("file", new Blob([file.target.result]), name);
+      await fetch("/api/put_soundfont", { method: "POST", body: formdata });
+      syncSoundfontList();
     },
-    body: JSON.stringify({filename})
-  })
-})
+  );
+});
+
+document
+  .getElementById("soundfontselection")
+  .addEventListener("change", (ev) => {
+    let el = document.getElementById("soundfontselection");
+    let filename;
+    if (el.value === "default") {
+      filename = null;
+    } else {
+      filename = el.value;
+    }
+    fetch("/api/set_soundfont", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ filename }),
+    });
+  });
--- www/pre.js
+++ www/pre.js
@@ -1,117 +1,122 @@
-
 var ListenerHacker = {
   listenerFuncs: {},
   listenerElems: {},
   origAddEventListener: window.addEventListener,
   createFakeListener: function (name, elem) {
     let th = window.ListenerHacker;
-    th.listenerFuncs[name] = {}
-    th.listenerElems[name] = elem
+    th.listenerFuncs[name] = {};
+    th.listenerElems[name] = elem;
     let newfunc = function (...args) {
       let my_listener_funcs = th.listenerFuncs[name];
       if (my_listener_funcs[args[0]] === undefined) {
-        my_listener_funcs[args[0]] = []
+        my_listener_funcs[args[0]] = [];
       }
-      my_listener_funcs[args[0]].push(args)
-      th.origAddEventListener.call(elem, ...args)
-    }
-    elem.addEventListener = newfunc
+      my_listener_funcs[args[0]].push(args);
+      th.origAddEventListener.call(elem, ...args);
+    };
+    elem.addEventListener = newfunc;
   },
   disableListeners: function (name, t) {
     let th = window.ListenerHacker;
-    if (th.listenerFuncs[name][t] === undefined) {return}
+    if (th.listenerFuncs[name][t] === undefined) {
+      return;
+    }
     for (let i = 0; i < th.listenerFuncs[name][t].length; i++) {
-      th.listenerElems[name].removeEventListener(...th.listenerFuncs[name][t][i])
+      th.listenerElems[name].removeEventListener(
+        ...th.listenerFuncs[name][t][i],
+      );
     }
   },
   enableListeners: function (name, t) {
     let th = window.ListenerHacker;
-    if (th.listenerFuncs[name][t] === undefined) {return}
+    if (th.listenerFuncs[name][t] === undefined) {
+      return;
+    }
     for (let i = 0; i < th.listenerFuncs[name][t].length; i++) {
-      th.origAddEventListener.call(th.listenerElems[name], ...th.listenerFuncs[name][t][i])
+      th.origAddEventListener.call(
+        th.listenerElems[name],
+        ...th.listenerFuncs[name][t][i],
+      );
     }
   },
   isActivated: false,
   activate: function () {
     let th = window.ListenerHacker;
-    th.createFakeListener('canvas', document.getElementById('canvas'));
+    th.createFakeListener("canvas", document.getElementById("canvas"));
     th.isActivated = true;
   },
   dispatchEvent: function (name, e) {
     let th = window.ListenerHacker;
-    th.listenerFuncs[name][e.type].forEach((x) => {x[1].call(th.listenerElems[name], e)})
+    th.listenerFuncs[name][e.type].forEach((x) => {
+      x[1].call(th.listenerElems[name], e);
+    });
   },
   pressKey: function (name, code) {
-    let e = new Event('keydown');
+    let e = new Event("keydown");
     e.code = code;
     this.dispatchEvent(name, e);
     setTimeout(() => {
-      let e = new Event('keyup');
+      let e = new Event("keyup");
       e.code = code;
       this.dispatchEvent(name, e);
-    }, 100)
-  }
-}
+    }, 100);
+  },
+};
 
 // ListenerHacker.activate()
 
-
-
-
-
-
-
-
-let keySet = new Set()
+let keySet = new Set();
 
 /**
-  * Update the set of pressed keys.
-  * @param {string} action specify whether to add or remove subset from pressed key list.
-  * @param {string} subset a subset of simulated key names, separated with comma
-  */
+ * Update the set of pressed keys.
+ * @param {string} action specify whether to add or remove subset from pressed key list.
+ * @param {string} subset a subset of simulated key names, separated with comma
+ */
 
 function updateSimulatedKeySet(action, subset) {
-  if (action === 'add') {
-    keySet = keySet.union(new Set(subset.split(',')));
-  } else if (action === 'remove') {
-    keySet = keySet.difference(new Set(subset.split(',')));
+  if (action === "add") {
+    keySet = keySet.union(new Set(subset.split(",")));
+  } else if (action === "remove") {
+    keySet = keySet.difference(new Set(subset.split(",")));
   }
-  executeKeySet()
+  executeKeySet();
 }
 
 let priv_keySet = keySet;
 
-function executeKeySet () {
-  if (!window.simulateKeyboardEvent) { return; }
-  let keyups = priv_keySet.difference(keySet)
-  let keydowns = keySet.difference(priv_keySet)
-  keyups.forEach(x => {
-    window.simulateKeyboardEvent('keyup', x)
-  })
-  keydowns.forEach(x => {
-    window.simulateKeyboardEvent('keydown', x)
-  })
+function executeKeySet() {
+  if (!window.simulateKeyboardEvent) {
+    return;
+  }
+  let keyups = priv_keySet.difference(keySet);
+  let keydowns = keySet.difference(priv_keySet);
+  keyups.forEach((x) => {
+    window.simulateKeyboardEvent("keyup", x);
+  });
+  keydowns.forEach((x) => {
+    window.simulateKeyboardEvent("keydown", x);
+  });
   priv_keySet = keySet;
 }
 
-let orig_console_log = console.log
+let orig_console_log = console.log;
 
 function server_put_log(...args) {
-    orig_console_log(args)
-    let s;
-    try {
-        s = JSON.stringify(args)
-    } catch (e) {
-        s = String(args)
-    }
-    fetch("/api/put_log", {
-      headers: {
-	"Content-Type": "application/json"	
-      },
-      method: "POST",
-      body: JSON.stringify({text: "=DEBUG=\n" + s + "\n", reset: false})
-    })
+  orig_console_log(args);
+  let s;
+  try {
+    s = JSON.stringify(args);
+  } catch (e) {
+    s = String(args);
+  }
+  fetch("/api/put_log", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({ text: "=DEBUG=\n" + s + "\n", reset: false }),
+  });
 }
 
-console.log = server_put_log
-console.error = server_put_log
+console.log = server_put_log;
+console.error = server_put_log;
